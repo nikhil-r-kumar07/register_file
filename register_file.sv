4d5a407c94969ef75135b3f79fda89171084b7be
nikhil-r-kumar07 <nikhil.r.kumar07@gmail.com>
2026-04-08 01:56:04 -0400

    create and update register_file.sv

---
+++ register_file.sv
@@ -0,0 +1,23 @@
+module register_file(
+    input logic clk,
+    input logic write_enable,
+    input logic [1:0] write_address,
+    input logic [3:0] write_data,
+    input logic [1:0] read_address_a,
+    input logic [1:0] read_address_b,
+    output logic [3:0] read_data_a,
+    output logic [3:0] read_data_b
+);
+
+logic [3:0] registers [4];
+
+always_ff @(posedge clk) begin
+    if (write_enable) begin
+        registers[write_address] <= write_data;
+    end
+end
+
+always_comb begin
+    read_data_a = registers[read_address_a];
+    read_data_b = registers[read_address_b];
+end
